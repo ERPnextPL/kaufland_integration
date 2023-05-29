9458d0f2c9b6fbe8ea3c2b638f7304917c3598ec
siasty3 <siasty3@gmail.com>
2023-05-29 15:24:48 +0200

    .

--- kaufland_integration/kaufland_integration/scheduler/kaufland.py
+++ kaufland_integration/kaufland_integration/scheduler/kaufland.py
@@ -6,9 +6,11 @@ from kaufland_integration.kaufland_integration.scheduler.Helper.jobs import add_
 def get_orders():
     last_log = frappe.get_last_doc("Scheduled Job Log", filters={"scheduled_job_type": "kaufland.get_orders", "status": "Start"}, order_by="creation desc")
     today = datetime.now().date().isoformat() + "T00:00:00Z" 
-    
+
     orders = get_orders_form_kaufland(today,last_log)
     if orders != None:
+        add_comment_to_job(last_log,f"Start process at: {datetime.now()} ")
         add_comment_to_job(last_log,f"Orders for date {today}: {str(orders)} ")
         for id in orders:           
             set_job_for_order_async(f"kaufland.get_order={id}",f"kaufland_integration.kaufland_integration.scheduler.Helper.orders.get_order_form_kaufland_by_id","default",id,last_log)
+    

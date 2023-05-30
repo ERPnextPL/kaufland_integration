87dea5a01aae62d5c1819f4705b81ffd0cf4ec15
siasty3 <siasty3@gmail.com>
2023-05-30 12:48:59 +0200

    gotowe

--- kaufland_integration/kaufland_integration/scheduler/after_uninstall.py
+++ kaufland_integration/kaufland_integration/scheduler/after_uninstall.py
@@ -1,9 +1,19 @@
 
+from kaufland_integration.kaufland_integration.scheduler.translations import translations
 from kaufland_integration.kaufland_integration.scheduler.Helper.erpnext.payment import Payment
 from kaufland_integration.kaufland_integration.scheduler.Helper.jobs import delete_all_jobs
 
 
 def uninstall():
+    #
     payment = Payment()
+    translationsObj = translations()
+
+    # delete payments
     payment.deletePaymentTerm()
+    
+    # delete translations
+    translationsObj.delete_translations(translationsObj.get_translation_list())
+    
+    # delete all jobs from queue
     delete_all_jobs()
---
+++ kaufland_integration/kaufland_integration/scheduler/translations.py
@@ -0,0 +1,30 @@
+import frappe
+
+
+class translations:
+    def add_translations(self, translations_list):
+
+        for items in translations_list:
+            translation = frappe.new_doc("Translation")
+            translation.update(items)
+            translation.insert()
+            
+    def delete_translations(self, translations_list):
+        for items in translations_list:
+            translation = frappe.get_doc("Translation",items)
+            frappe.db.delete("Translation", translation.name)
+        frappe.clear_cache()
+            
+    def get_translation_list(self):
+        return [
+            {
+                "source_text": "Secret key",
+                "language": "pl",
+                "translated_text": "Sekretny klucz"
+            },
+            {
+                "source_text": "Number of days back",
+                "language": "pl",
+                "translated_text": "Liczba dni wstecz"
+            }
+        ]
